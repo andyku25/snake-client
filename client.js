969154a552f5feb8172d0c5ded556da7efcec80f
Andrew <andrew.ku93@gmail.com>
2021-02-10 17:47:06 +0000

    create new client.js file and confirm client connection to server

---
+++ client.js
@@ -0,0 +1,28 @@
+const net = require("net");
+
+
+const connect = () => {
+  const conn = net.createConnection({
+    host: "135.23.222.131",
+    port: 50542
+  })
+  // interpret incoming data
+  conn.setEncoding("utf8");
+
+  conn.on("connect", () => {
+    console.log("Snake Connection was Successful!");
+    const name = "AKU";
+    conn.write("Name: AKU");
+  })
+
+  conn.on("data", (data) => {
+    console.log(data);
+  })
+
+
+  return conn;
+};
+
+module.exports = {
+  connect
+};
--- play.js
+++ play.js
@@ -1,21 +1,4 @@
-const net = require("net");
-
-
-const connect = () => {
-  const conn = net.createConnection({
-    host: "135.23.222.131",
-    port: 50542
-  })
-  // interpret incoming data
-  conn.setEncoding("utf8");
-
-  conn.on("data", (data) => {
-    console.log(data);
-    
-  })
-
-  return conn;
-};
+const { connect } = require("./client");
 
 console.log("Connecting...");
 connect();
