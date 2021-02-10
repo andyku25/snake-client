e0c6503ad11029486717770f924cb0c08d756d9c
Andrew <andrew.ku93@gmail.com>
2021-02-10 18:21:12 +0000

    moved input setup and handler function to new input file

--- play.js
+++ play.js
@@ -1,19 +1,6 @@
 const { connect } = require("./client");
+const { setupInput } = require("./input");
 
-const setupInput = () => {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  stdin.on("data", (key) => {
-    if (key === '\u0003') {
-      process.exit();
-    }
-  });
-
-  return stdin;
-}
 
 console.log("Connecting...");
 connect();
